d5ed87cdad1b699854ee47828626e1f2f5b14182
lee <shirlin@fofo.tw>
2020-07-28 00:06:42 +0800

    wip-home

---
+++ src/assets/js/env.js
@@ -0,0 +1,3 @@
+module.exports = {
+    domain: 'https://e7friendly.eden.org.tw',
+};
--- src/assets/js/story.js
+++ src/assets/js/story.js
@@ -1,5 +1,6 @@
 import mode from './modules/mode';
 import { story_details as stories } from './story_details';
+import { domain } from './env';
 
 new Vue({
     el: '#storyPage',
@@ -13,13 +14,45 @@ new Vue({
             location: { hash },
         } = window;
         const [tag, num] = hash.split('#');
-        this.story_type = Number(num) - 1;
+        if (!!Number(num)) {
+            this.story_type = Number(num) - 1;
+        }
         this.story_individual = this.stories[this.story_type];
-        console.log(this.stories);
     },
     mounted: function () {
         mode();
-        this.$nextTick(() => {});
+        this.$nextTick(() => {
+            this.getVoteNumber();
+        });
     },
-    methods: {},
+    methods: {
+        getVoteNumber() {
+            axios
+                .get(`${domain}/api/GetPersons.php`)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    // 请求失败处理
+                    console.warn(error);
+                });
+        },
+        voting() {
+            axios
+                .post(`${domain}/api/CountPerson.php`, {
+                    type: this.story_type,
+                })
+                .then((response) => {
+                    console.log(response);
+                    Cookies.set(`type${this.story_type}`, 'value', {
+                        expires: 7,
+                        path: '',
+                    });
+                })
+                .catch(function (error) {
+                    // 请求失败处理
+                    console.warn(error);
+                });
+        },
+    },
 });
